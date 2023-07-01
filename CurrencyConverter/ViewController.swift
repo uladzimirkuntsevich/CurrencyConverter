21deab37bad7be2d08b230dd2699f47be433270c
uladzimirkuntsevich <uladzimir.kuntsevich.rs@gmail.com>
2023-07-01 19:34:20 +0400

    add new elements to ViewController

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var textField = UITextField()
+    var labelField = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addSubviews()
+        addElementConstraints()
+        addStyles()
     }
-
+    
+    func addSubviews() {
+        [textField, labelField].forEach({view.addSubview($0)})
+        [textField, labelField].forEach({$0.translatesAutoresizingMaskIntoConstraints = false})
+    }
+    
+    func addElementConstraints() {
+        view.addConstraints([
+            //MARK: adding constraints for textField
+            textField.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            //MARK: adding constraints for labelField
+            labelField.topAnchor.constraint(equalTo: textField.topAnchor, constant: 100),
+            labelField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            labelField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+ 
+            
+        ])
+    }
+    
+    
+    func addStyles(){
+        textField.placeholder = "0.00"
+        labelField.text = "0.00"
+        [textField, labelField].forEach({$0.layer.borderWidth = 1})
+        [textField, labelField].forEach({$0.layer.cornerRadius = 5})
+        [textField, labelField].forEach({$0.layer.borderColor = UIColor.black.cgColor})
+    }
 
 }
 
